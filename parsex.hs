27e5f936509608d0e63fa6de820f04cae8e8b537
GinBaby <jdog74@gmail.com>
2017-01-08 01:58:12 -0600

    add gitignore, stackify optex

--- parsex.hs
+++ parsex.hs
@@ -1,1 +1,4 @@
 -- parsing examples: applicative/alternative, monadic
+
+-- be sure to get slides, image files, and a top-level haskell .gitignore here before you build anythign with stack
+-- be sure to put a separate .gitignore into the project just in fucking case
